c6f2405c562bee8a3e377de44a3f7b2583ce7f79
juven0 <bocquelin@gmail.com>
2025-02-24 14:57:50 +0300

    algo ok

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import mainLop, { position } from "./algo";
 
-const iteration = 1;
-const timeSleep = 6000;
+const iteration = 2;
+const timeSleep = 2000;
 
 function delay(ms: number) {
   return new Promise((resolve) => setTimeout(resolve, ms));
@@ -13,28 +13,31 @@ function App() {
   let Gp = [
     { x: 10, y: 10 },
     { x: 11, y: 11 },
+    { x: 12, y: 11 },
+    { x: 11, y: 12 },
     { x: 10, y: 12 },
   ];
-  // const [Ga, setGa] = useState<position[]>([]);
+  const [Ga, setGa] = useState<position[]>(Gp);
   const cellSize = 10;
   const numCell = 70;
 
-  // async function loopWithDelay() {
-  //   for (let i = 0; i < iteration; i++) {
-  //     const Gf = mainLop(69, Gp);
-  //     Gp = Gf;
-  //     await delay(timeSleep);
-  //   }
-  // }
-  mainLop(69, Gp);
-  // loopWithDelay();
-  // console.log(Gp);
+  async function loopWithDelay() {
+    for (let i = 0; i < iteration; i++) {
+      await delay(timeSleep);
+    }
+  }
+  loopWithDelay();
+
+  const next = () => {
+    const Gf = mainLop(69, Ga);
+    setGa(Gf);
+  };
 
   return (
     <div className="container">
-      {/* <svg width={700} height={700}>
-        {Array.from({ length: numCell }).map((_, col) =>
-          Array.from({ length: numCell }).map((_, row) => {
+      <svg width={700} height={700}>
+        {Array.from({ length: numCell }).map((_, row) =>
+          Array.from({ length: numCell }).map((_, col) => {
             const cellId = `${row}-${col}`;
             return (
               <rect
@@ -44,14 +47,15 @@ function App() {
                 height={cellSize}
                 width={cellSize}
                 fill={
-                  Gp.find((e) => e.x === row && e.y === col) ? "black" : "white"
+                  Ga.find((e) => e.x === row && e.y === col) ? "black" : "white"
                 }
                 className="cell"
               />
             );
           })
         )}
-      </svg> */}
+      </svg>
+      <button onClick={() => next()}>next</button>
     </div>
   );
 }
--- src/algo.ts
+++ src/algo.ts
@@ -5,38 +5,35 @@ export type position = {
 
 const mainLop = (n: number, G: position[]) => {
   let Gp = G;
-  for (let d = 0; d < 1; d++) {
-    const Ga = Gp;
-    for (let i = 10; i < 13; i++) {
-      for (let j = 10; j < 13; j++) {
-        let nv: position[] = [];
-        for (let k = i - 1; k <= i + 1; k++) {
-          for (let l = j - 1; l <= j + 1; l++) {
-            const a = Ga.find((e) => e.x === k && e.y === l);
-            if (a !== undefined) {
-              nv = [...nv, a];
-              //   console.log(nv);
-            }
-          }
-          console.log();
-        }
-        const a = Ga.find((e) => e.x === i && e.y === j);
-        if (a !== undefined) {
-          if (nv.length === 2 || nv.length === 3) {
-            Gp = [...Gp, a];
-          } else {
-            Gp = Gp.filter((e) => !(e.x === i && e.y === j));
-          }
-        } else {
-          if (nv.length === 3) {
-            console.log("ao");
-            Gp = [...Gp, { x: i, y: j }];
+  const Ga = Gp;
+  for (let i = 0; i < n; i++) {
+    for (let j = 0; j < n; j++) {
+      let nv: position[] = [];
+      for (let k = i - 1; k <= i + 1; k++) {
+        for (let l = j - 1; l <= j + 1; l++) {
+          const a = Ga.find((e) => e.x === k && e.y === l);
+          if (a !== undefined) {
+            nv = [...nv, a];
+            nv = nv.filter((e) => !(e.x === i && e.y === j));
           }
         }
       }
+      const a = Ga.find((e) => e.x === i && e.y === j);
+      if (a !== undefined) {
+        if (nv.length === 2 || nv.length === 3) {
+          Gp = Gp.filter((e) => !(e.x === a.x && e.y === a.y));
+          Gp = [...Gp, a];
+        } else {
+          Gp = Gp.filter((e) => !(e.x === i && e.y === j));
+        }
+      } else {
+        if (nv.length === 3) {
+          Gp = [...Gp, { x: i, y: j }];
+        }
+      }
     }
-    console.log(Gp);
   }
+  return Gp;
 };
 
 export default mainLop;
