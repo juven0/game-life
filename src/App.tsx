0d4816caf5f616b6c30499372af0761c5ed4ec42
juven0 <bocquelin@gmail.com>
2025-02-24 12:06:12 +0300

    core

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,38 @@
+import { useEffect, useState } from "react";
 import "./App.css";
+import mainLop, { position } from "./algo";
+
+const iteration = 1;
+const timeSleep = 6000;
+
+function delay(ms: number) {
+  return new Promise((resolve) => setTimeout(resolve, ms));
+}
 
 function App() {
+  let Gp = [
+    { x: 10, y: 10 },
+    { x: 11, y: 11 },
+    { x: 10, y: 12 },
+  ];
+  // const [Ga, setGa] = useState<position[]>([]);
   const cellSize = 10;
-  const numCell = 200;
+  const numCell = 70;
+
+  // async function loopWithDelay() {
+  //   for (let i = 0; i < iteration; i++) {
+  //     const Gf = mainLop(69, Gp);
+  //     Gp = Gf;
+  //     await delay(timeSleep);
+  //   }
+  // }
+  mainLop(69, Gp);
+  // loopWithDelay();
+  // console.log(Gp);
 
   return (
     <div className="container">
-      <svg width={900} height={900}>
+      {/* <svg width={700} height={700}>
         {Array.from({ length: numCell }).map((_, col) =>
           Array.from({ length: numCell }).map((_, row) => {
             const cellId = `${row}-${col}`;
@@ -17,13 +43,15 @@ function App() {
                 y={row * cellSize}
                 height={cellSize}
                 width={cellSize}
-                fill={col == 10 && row == 10 ? "black" : "white"}
+                fill={
+                  Gp.find((e) => e.x === row && e.y === col) ? "black" : "white"
+                }
                 className="cell"
               />
             );
           })
         )}
-      </svg>
+      </svg> */}
     </div>
   );
 }
--- src/algo.ts
+++ src/algo.ts
@@ -1,24 +1,42 @@
+export type position = {
+  x: number;
+  y: number;
+};
 
-
-type position = {
-    x: number
-    y: number
-}
-
-const mainLop = (n: number, duration: number): [position]=> {
-    let nv: [position] = []
-
-    for (let t = 0; t < duration; t++) {
-        for (let i = 0; i < n; i++) {
-            for (let j = 0; j < n; j++) {
-                for (let k = i-1; k < i+1; k++) {
-                    for (let l = j-1; l < j+1; l++) {
-
-                    }
-                }
+const mainLop = (n: number, G: position[]) => {
+  let Gp = G;
+  for (let d = 0; d < 1; d++) {
+    const Ga = Gp;
+    for (let i = 10; i < 13; i++) {
+      for (let j = 10; j < 13; j++) {
+        let nv: position[] = [];
+        for (let k = i - 1; k <= i + 1; k++) {
+          for (let l = j - 1; l <= j + 1; l++) {
+            const a = Ga.find((e) => e.x === k && e.y === l);
+            if (a !== undefined) {
+              nv = [...nv, a];
+              //   console.log(nv);
             }
+          }
+          console.log();
         }
+        const a = Ga.find((e) => e.x === i && e.y === j);
+        if (a !== undefined) {
+          if (nv.length === 2 || nv.length === 3) {
+            Gp = [...Gp, a];
+          } else {
+            Gp = Gp.filter((e) => !(e.x === i && e.y === j));
+          }
+        } else {
+          if (nv.length === 3) {
+            console.log("ao");
+            Gp = [...Gp, { x: i, y: j }];
+          }
+        }
+      }
     }
+    console.log(Gp);
+  }
+};
 
-    return nv
-}
+export default mainLop;
