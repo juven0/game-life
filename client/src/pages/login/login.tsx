8fe887622fa5c8a7e0f825364e0949083961e4ce
juven0 <bocquelin@gmail.com>
2025-04-03 16:27:37 +0300

    login page done

--- client/src/pages/login/login.tsx
+++ client/src/pages/login/login.tsx
@@ -1,9 +1,35 @@
 import "./login.scss";
+import logo from "../../assets/image/gamelife.png";
 
 const Login = () => {
   return (
     <div className="Login">
-      <h1>hello from login </h1>
+      <div className="leftSide">
+        <div className="logo">
+          <img src={logo} alt="logo" />
+          <p>Game Life</p>
+        </div>
+        <div className="text">
+          <p>
+            Welcome to the fascinating world of cellular automata, where each
+            cell follows simple rules to create astonishingly complex patterns!
+          </p>
+        </div>
+      </div>
+
+      <div className="rightSide">
+        <div className="form">
+          <div className="item-form">
+            <label htmlFor="">Username</label>
+            <input type="text" name="" id="" />
+          </div>
+          <div className="item-form">
+            <label htmlFor="">Room ID</label>
+            <input type="text" name="" id="" />
+          </div>
+          <button>Join Parti</button>
+        </div>
+      </div>
     </div>
   );
 };
